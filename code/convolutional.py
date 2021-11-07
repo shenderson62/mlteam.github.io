429815c6179cb30ff11eed99c9700c42fd1a967b
Jett Qiu <jett.zqiu@gmail.com>
2021-11-07 15:53:08 -0500

    Feat: Creates convolutional layer
    Adds code for convolutional layer

---
+++ code/convolutional.py
@@ -0,0 +1,11 @@
+from keras.models import Sequential
+from keras.layers import Conv2D
+
+def convolutional(num_filters = 8, filter_size = 3):
+    conLayer = Conv2D(num_filters,
+                    filter_size,
+                    input_shape=(28, 28, 1),
+                    strides=2,
+                    padding='same',
+                    activation='relu',)
+    return conLayer
